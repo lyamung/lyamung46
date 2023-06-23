969bf909cbb75859ef2629525545a222f5ed59c6
lyamung <137476067+lyamung@users.noreply.github.com>
2023-06-23 18:31:14 +0900

    수정3

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ openPopupButton.addEventListener("click", function () {
 });
 
 submitButton.addEventListener("click", function () {
-    var password = "패스워드를 여기에 입력하세요";
+    var password = "0530"; // 패스워드를 여기에 입력하세요
 
     if (passwordInput.value === password) {
         passwordPopup.style.display = "none";
-        projectLink.click();
+        window.location.href = projectLink.href; // 링크로 이동하는 대신 페이지를 새로고침하여 링크를 열어보세요.
     } else {
         alert("잘못된 패스워드입니다.");
     }
